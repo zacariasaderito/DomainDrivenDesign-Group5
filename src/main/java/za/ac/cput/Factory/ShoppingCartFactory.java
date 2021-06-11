333d6a6af6251dd4ba97ec5dbe0020480f354349
Christopher <christopher.doolings@gmail.com>
2021-06-11 19:47:13 +0200

    ShoppingCartFactory class added + Test class

---
+++ src/main/java/za/ac/cput/Factory/ShoppingCartFactory.java
@@ -0,0 +1,26 @@
+/* ShoppingCartFactory.java
+   Factory class for ShoppingCart
+   Author: Christopher Doolings
+   Date: 9 June 2021
+ */
+
+package za.ac.cput.Factory;
+
+import za.ac.cput.Entity.ShoppingCart;
+
+import java.util.UUID;
+
+public class ShoppingCartFactory {
+    public static ShoppingCart createShoppingCart(int quantity, double orderPrice) {
+        String customerId = UUID.randomUUID().toString();
+        String productId = UUID.randomUUID().toString();
+        ShoppingCart shoppingCart = new ShoppingCart.Builder()
+                .customerId(customerId)
+                .productId(productId)
+                .quantity(quantity)
+                .orderPrice(orderPrice)
+                .build();
+
+        return shoppingCart;
+    }
+}
