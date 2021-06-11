5f23bd6142dd6de3ec47f81459b7ea6a25607904
Chadleigh Dunbar <dunbarchadleigh@gmail.com>
2021-06-11 11:49:07 +0200

    First commit

---
+++ src/main/java/za/ac/cput/Entity/Payment.java
@@ -0,0 +1,46 @@
+package za.ac.cput.Entity;
+
+public class Payment{
+
+    private String paymentId;
+    private String paymentType;
+
+    /** public String confirmPayment;
+     * public String cancelPayment;
+     * @param builder
+     */
+
+    private Payment(Builder builder){
+        this.paymentId = builder.paymentId;
+        this.paymentType = builder.paymentType;
+    }
+
+    public static class Builder{
+        private String paymentId;
+        private String paymentType;
+
+        public Builder PaymentId(String paymentId) {
+            this.paymentId = paymentId;
+            return this;
+        }
+
+        public Builder PaymentType(String paymentType) {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public Payment build(){
+            return new Payment(this);
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "paymentId=" + paymentId +
+                ", paymentType='" + paymentType + '\'' +
+                '}';
+    }
+}
