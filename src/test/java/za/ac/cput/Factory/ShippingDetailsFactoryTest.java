0554418e17a70f74b6d1440fa9f848f62b7fa420
zacariasaderito <aderitozacarias123@hotmail.com>
2021-06-11 23:01:03 +0200

    ShippingDetails Entity + Factory + FactoryTest

--- src/test/java/za/ac/cput/Factory/ShippingDetailsFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/ShippingDetailsFactoryTest.java
@@ -1,5 +1,13 @@
 package za.ac.cput.Factory;
 
+/**
+ * ShippingDetailsFactory.java
+ * Shipping Details Factory
+ * Author: Aderito Zacarias (215278739)
+ * 11 June 2021
+ **/
+
+
 import org.junit.jupiter.api.Test;
 import za.ac.cput.Entity.ShippingDetails;
 
